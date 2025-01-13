e87421393f811cfd4fddd73315a198907beab4d2
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-01-13 05:32:59 +0200

    Defined custom <UserAccount> model that overrides the default <AbstractUser> model that comes with django.

--- system/models.py
+++ system/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
 
-# Create your models here.
+
+class UserAccount(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    email = models.EmailField(unique=True)
+    MANAGER_ROLE = 'Manager'
+    ADMIN_ROLE = 'Admin'
+    EMPLOYEE_ROLE = 'Employee'
+    ROLE_CHOICES = (
+        (ADMIN_ROLE, 'Admin'),
+        (MANAGER_ROLE, 'Manager'),
+        (EMPLOYEE_ROLE, 'Employee'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=EMPLOYEE_ROLE)
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name = "User Account"
+        verbose_name_plural = "User Accounts"
