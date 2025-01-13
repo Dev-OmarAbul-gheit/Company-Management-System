d3bca7ab0e6cb7070ee04edd7ad5e258f5d4056b
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-01-13 10:31:54 +0200

    Created custom permission for each role

---
+++ system/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import BasePermission 
+
+
+class IsAdmin(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'Admin'
+
+
+class IsManager(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'Manager'
+
+
+class IsEmployee(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'Employee'
